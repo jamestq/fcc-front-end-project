9611465c9fd3503bf8b62e5b0dfc813488305624
jamestq <57053746+jamestq@users.noreply.github.com>
2022-12-29 10:18:23 +1100

    move routes to render under App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import { Outlet } from 'react-router';
+import "./App.css";
 
 export default class App extends React.Component{
     render(){
-        return <h1>Hello World</h1>;
+        return <div className="page">
+            <h1>Main App Page</h1>
+            <main className="container">
+                <Outlet/>
+            </main>
+        </div>;
     }
 }
 
