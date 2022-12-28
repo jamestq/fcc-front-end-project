07e43ac94f1348f2232d4a2a314bd7f1c7b95b9b
jamestq <57053746+jamestq@users.noreply.github.com>
2022-12-28 17:41:47 +1100

    add routing and components' fil structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
+import ErrorPage from './ErrorPage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    errorElement: <ErrorPage/>
+  },
+  {
+    
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
