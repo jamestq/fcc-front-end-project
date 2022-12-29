a0a1f140e3beb9cbc679ee401c2dfcb7ecd7f8bd
jamestq <57053746+jamestq@users.noreply.github.com>
2022-12-29 13:46:14 +1100

    add basic functions to handle markdown parsing.

--- src/components/MarkdownPreview.jsx
+++ src/components/MarkdownPreview.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import {marked} from 'marked';
+import sanitizeHtml from 'sanitize-html';
+import parse from 'html-react-parser';
 
 export default class MarkdownPreivew extends React.Component{
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            input: '',
+            output: '',
+        }
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event){
+        this.setState({
+            input: event.target.value,
+            output: parse(sanitizeHtml(marked.parse(event.target.value)))
+        })
+    }
+
     render(){
-        return <h1>This is Markdown Preview</h1>
+
+        return <div id="markdown-preview">
+            <textarea name="editor" id="editor" cols="30" rows="10" onChange={(event) => this.handleChange(event)}  value={this.state.input}>
+            </textarea>
+            <div id="preview">{this.state.output}</div>
+        </div>
     }
 }
